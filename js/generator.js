9349791d58c695c77470bd1512245e8d632fe17b
Jared Barboza <codeimpossible@users.noreply.github.com>
2021-04-24 21:52:07 -0400

    Create generator.js

---
+++ js/generator.js
@@ -0,0 +1,34 @@
+const suffixes = [
+  'borough',
+  'burg',
+  'bury',
+  'ter',
+  'town',
+  'minster',
+  'ston',
+  'field',
+  'ham',
+  ' Plains',
+];
+
+const words = [
+  'Packy',
+  'Wicked',
+  'Pissah',
+  'Dunk',
+  'Dirty',
+  'Sling',
+  'Wang',
+  'Junk',
+];
+
+const prefixes = [
+  'North',
+  'South',
+  'East',
+  'West',
+  'Charles',
+  'Vicks',
+  'Fitch',
+  'Leo',
+];
